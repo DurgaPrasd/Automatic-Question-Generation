f83e9ab6583149624eb7ec1328b2650187b1db5b
wb <519369865@qq.com>
2016-11-16 17:53:03 +0100

    solve bug on ci requirements

--- aqg/app.py
+++ aqg/app.py
@@ -1,7 +1,10 @@
 from sentence_selection import SentenceSelection
 from gap_selection import GapSelection
+from feature_construction import FeatureConstruction
 
 ss = SentenceSelection()
 sentences = ss.prepare_sentences("/Users/wbcha/Desktop/project/Automatic_Question_Generation/tests/obama.txt")
 gs = GapSelection()
-print gs.get_candidates(sentences)
+candidates = gs.get_candidates(sentences)
+# fc = FeatureConstruction()
+# fc.built_features(candidates)
