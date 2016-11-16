fdbd128e95426de7924bc5162f5fa64c97efcd32
wb <519369865@qq.com>
2016-11-16 17:53:03 +0100

    solve bug on ci requirements

--- aqg/feature_construction.py
+++ aqg/feature_construction.py
@@ -1,9 +1,6 @@
 from __init__ import *
 
-class Feature_Construction:
-	def __init__(self):
-		pass
-
+class FeatureConstruction:
 	def _num_tokens_in_answer(self, row):
 	    """Count number of tokens in answer
 	    Args:
@@ -27,12 +24,12 @@ class Feature_Construction:
 		    row(pandas.dataframe): number of tokens in sentence (question)
 		"""
 		question = row.Question
-	    try:
-	        row['Num_Tokens_In_Sentence'] = len(question.split())
-	        return row
-	    except:
-	        row['Num_Tokens_In_Sentence'] = 0
-	    	return row
+        try:
+        	row['Num_Tokens_In_Sentence'] = len(question.split())
+        	return row
+        except:
+        	row['Num_Tokens_In_Sentence'] = 0
+        	return row
 
 	def _num_row_tokens_matching_in_out(self, row):
 		"""Number of tokens in the answer that match tokens outside of the answer
@@ -83,4 +80,38 @@ class Feature_Construction:
 	    except:
 	        row['Percentage_Token_In_Out_Answer'] = 0
 	        return row
+
+    def built_features(self, candidates):
+    	"""Build features for candidate question answer pairs
+    	Args:
+    	    candidates(list of dict): list of dictionary, e.g.
+			[{'Sentence': .....,'Question':.....,'Answer':...},...]
+		Returns:
+			df(pandas.dataframe): dataframe of question/answer/sentence/features 
+    	"""
+    	df = pd.DataFrame(candidates) #transform to dataframe
+    	for idx, row in df.iterrows():
+    		row = self._num_tokens_in_answer(row)
+    		row = self._num_token_in_sentence(row)
+    		row = self._num_row_tokens_matching_in_out(row)
+    		row = self._percentage_token_in_answer(row)
+    		row = self._percentage_token_in_out_answer(row)
+    		print idx
+    		print row
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
