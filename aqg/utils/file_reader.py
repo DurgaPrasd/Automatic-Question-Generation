1566e969ba2caae48ed408636a94888cd9c51a09
wb <519369865@qq.com>
2017-04-24 18:01:14 +0200

    update pep8 style

--- aqg/utils/file_reader.py
+++ aqg/utils/file_reader.py
@@ -1,7 +1,9 @@
 from os import listdir
 from os.path import isfile, join
 
+
 class File_Reader:
+
     def __init__(self):
         pass
 
--- aqg/utils/file_writer.py
+++ aqg/utils/file_writer.py
@@ -3,9 +3,11 @@ import json
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
+
 class File_Writer:
+
     def __init__(self):
-        self.candidate_path = os.environ.get("CANDIDATE_PATH") 
+        self.candidate_path = os.environ.get("CANDIDATE_PATH")
 
     def write_candidate_questions(self, content, file_name):
         '''
@@ -13,4 +15,4 @@ class File_Writer:
         @arg content: content need to write
         '''
         with open(self.candidate_path + file_name, 'w') as c:
-            json.dump(content,c)
+            json.dump(content, c)
